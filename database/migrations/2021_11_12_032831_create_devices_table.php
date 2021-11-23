b6a0ee24a74bcd7bd7251b59365d3b2a1e02c453
Ryan Kidd <kidd@crowdfavoritecom>
2021-11-23 12:36:43 -0800

    Force innodb

--- database/migrations/2021_11_12_032831_create_devices_table.php
+++ database/migrations/2021_11_12_032831_create_devices_table.php
@@ -14,6 +14,7 @@ class CreateDevicesTable extends Migration
     public function up()
     {
         Schema::create('devices', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id()->unsignedBigInteger();
             $table->string('name');
             $table->string('slug')->unique();
